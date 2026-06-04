6bd88d84fb3478f289419463d1b74ced1a93c4f7
PeterHgg <peterli427fh@163.com>
2026-06-04 10:49:15 +0800

    fix(clash): remove default dns config

--- sub/clashService.go
+++ sub/clashService.go
@@ -87,16 +87,6 @@ func (s *ClashService) GenerateClashConfig(uuid, password, cdnDomain string, cou
 			Tracing:       false,
 			Interval:      12, // 12小时自动更新
 		},
-		DNS: ClashDNS{
-			Enable:       true,
-			IPv6:         false,
-			EnhancedMode: "fake-ip",
-			FakeIPRange:  "198.18.0.1/16",
-			Nameserver: []string{
-				"223.5.5.5",
-				"119.29.29.29",
-			},
-		},
 		Proxies:       allProxies,
 		ProxyGroups:   proxyGroups,
 		RuleProviders: ruleProviders,
